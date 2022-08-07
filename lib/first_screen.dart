e56e99968eb376cefbab1dedef25b53dd24637d6
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2022-08-07 15:35:52 +0430

    create main file

---
+++ lib/first_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(
+        "hello world",
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+}
